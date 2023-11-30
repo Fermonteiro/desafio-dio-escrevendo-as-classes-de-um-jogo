e592a2bac50917b4d6abad94debdfdf84cb75b10
fermonteiro <fer.monteiro.works@gmail.com>
2023-11-30 15:14:18 -0300

    Subindo desafio finalizado

--- desafioFelipao.js
+++ desafioFelipao.js
@@ -1,30 +1,36 @@
-let vitorias = 50
-let derrotas = 30
-let dadosDoHeroi = calculadoraDePartidasRanqueadas(vitorias, derrotas)
-let classificacaoDoHeroi = definindoClassificacao(dadosDoHeroi)
-
-function calculadoraDePartidasRanqueadas(vit, der){
-    return vit - der
-}
-
-function definindoClassificacao(dados){
-    if(dados <= 10){
-        classificacao = "Ferro"
-    }else if(dados > 10 && dados <= 20){
-        classificacao = "Bronze"
-    }else if(dados > 20 && dados <= 50){
-        classificacao = "Prata"
-    }else if(dados > 50 && dados <= 80){
-        classificacao = "Ouro"
-    }else if(dados > 80 && dados <= 90){
-        classificacao = "Diamante"
-    }else if(dados > 90 && dados <= 100){
-        classificacao = "Lendario"
-    }else if(dados >= 100){
-        classificacao = "Imortal"
+class Hero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
     }
-
-    return classificacao
+  
+    atacar() {
+        let atack;
+  
+        switch (this.tipo) {
+            case 'mago':
+                atack = 'magia';
+                break;
+            case 'guerreiro':
+                atack = 'espada';
+                break;
+            case 'monge':
+                atack = 'artes marciais';
+                break;
+            case 'ninja':
+                atack = 'shuriken';
+                break;
+            default:
+                atack = 'um ataque indefinido';
+        }
+  
+        console.log(`O ${this.tipo} atacou usando ${atack}`);
+    }
 }
-
-console.log(`O Herói tem de saldo de ${dadosDoHeroi} está no nível de ${classificacaoDoHeroi}`)
+  
+const hero1 = new Hero('Fernando', 13, 'monge');
+const hero2 = new Hero('Gutemberg', 27, 'ninja');
+  
+hero1.atacar();
+hero2.atacar();
